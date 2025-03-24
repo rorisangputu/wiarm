4c060f488c50c36e68eb59939fa0e78680012dce
Rorisang <rorisangputu@gmail.com>
2025-03-24 11:21:48 +0200

    importing router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import Layout from './layouts/Layout'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Layout><Home/></Layout>} />
+      </Routes>
     </div>
   )
 }
