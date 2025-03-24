f41c7eabf53ceecdde5f6b696c81fe0415ce3aff
Rorisang <rorisangputu@gmail.com>
2025-03-24 14:50:28 +0200

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import cab from './../assets/cab.jpg'
 
 const Hero = () => {
   return (
-    <div className='bg-[#283618]'>Hero</div>
+    <div className='w-full '>
+        <div className='bg-[#3f5e1c] w-[70%]  mx-auto flex'>
+            <div className='flex justify-center items-center p-10 w-1/2'>
+                <h1 className='uppercase text-white font-bold text-4xl'>Welcome to family farm</h1>
+            </div>
+            <div className='w-1/2'>
+                <img src={cab} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
