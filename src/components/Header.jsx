a53f628d17cf7efe0af771ec46d10028d7c03b31
Rorisang <rorisangputu@gmail.com>
2025-04-03 15:03:54 +0200

    making changes to header and adding logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import corn from '../assets/corn_img.png'
 
 const Header = () => {
   return (
     <div className="w-full py-4">
-      <div className="w-[70%] mx-auto flex flex-row justify-between items-center">
-        <div className="flex items-center">
+      <div className="w-[70%] mx-auto flex justify-between items-center">
+          {/* Logo */}
           <Link to={"/"}>
-            <h1 className="font-semibold text-2xl">WiARM</h1>
+            <div className="flex flex-col items-center gap-1">
+              <img src={corn} alt="" className="w-7 h-7 text-[#283618]" /> 
+              <h1 className="font-semibold text-md">WiARM</h1>
+            </div>
           </Link>
-          <p className="px-2 text-xl">|</p>
+
+          {/* Links */}
+
+          <div>
           <ul className="flex gap-4">
             {links.map((link) => (
-              <li key={link.url} className="text-xl">
+              <li key={link.url} className="text-xl font-medium">
                 <Link to={link.url}>{link.title}</Link>
               </li>
             ))}
           </ul>
-        </div>
-        <div className="bg-[#283618] flex items-center p-3">
-            <p className="text-[#fefae0] text-lg font-normal">Contact Us</p>
-        </div>
+          </div>
+
+          {/* Contact us */}
+          <div className="bg-[#283618] flex items-center rounded-3xl py-3 px-5">
+            <p className="text-[#fefae0] text-normal font-normal">Get in touch</p>
+          </div>
+        
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,7 @@ import cab from './../assets/cab.jpg'
 const Hero = () => {
   return (
     <div className='w-full '>
-        <div className='bg-[#3f5e1c] w-[70%]  mx-auto flex'>
-            <div className='flex justify-center items-center p-10 w-1/2'>
-                <h1 className='uppercase text-white font-bold text-4xl'>Welcome to family farm</h1>
-            </div>
-            <div className='w-1/2'>
-                <img src={cab} alt="" />
-            </div>
-        </div>
+      
     </div>
   )
 }
