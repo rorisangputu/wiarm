2d8857a348d1de47ce7e6ee46cad8303c9f0a0ca
Rorisang <rorisangputu@gmail.com>
2025-04-03 15:39:31 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,31 +6,29 @@ const Header = () => {
   return (
     <div className="w-full py-4">
       <div className="flex justify-between items-center">
-          {/* Logo */}
-          <Link to={"/"}>
-            <div className="flex flex-col items-center gap-1">
-              <img src={corn} alt="" className="w-7 h-7 text-[#46973B]" /> 
-              <h1 className="font-semibold text-md">WiARM</h1>
-            </div>
-          </Link>
+        {/* Logo */}
+        <Link to={"/"}>
+          <div className="flex flex-col items-center gap-1">
+            <img src={corn} alt="" className="w-7 h-7 text-[#46973B]" /> 
+            <h1 className="font-semibold text-md">WiARM</h1>
+          </div>
+        </Link>
 
-          {/* Links */}
-
-          <div>
+        {/* Links */}
+        <div>
           <ul className="flex gap-4">
             {links.map((link) => (
-              <li key={link.url} className="text-xl font-medium">
-                <Link to={link.url}>{link.title}</Link>
-              </li>
+            <li key={link.url} className="text-xl font-medium">
+              <Link to={link.url}>{link.title}</Link>
+            </li>
             ))}
           </ul>
-          </div>
+        </div>
 
-          {/* Contact us */}
-          <div className="bg-[#46973B] flex items-center rounded-3xl py-3 px-5">
-            <p className="text-[#fefae0] text-normal font-normal">Get in touch</p>
-          </div>
-        
+        {/* Contact us */}
+        <div className="bg-[#46973B] flex items-center rounded-3xl py-3 px-5">
+          <p className="text-[#fefae0] text-normal font-normal">Get in touch</p>
+        </div>
       </div>
     </div>
   );
