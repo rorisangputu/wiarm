741aba9872fcfbb75f1ddf3c601bb4ea194ebee9
Rorisang <rorisangputu@gmail.com>
2025-04-04 14:26:06 +0200

    adding new section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import AboutSection from '../components/AboutSection'
+import Pillars from '../components/Pillars'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <AboutSection/>
+      <Pillars/>
     </div>
   )
 }
