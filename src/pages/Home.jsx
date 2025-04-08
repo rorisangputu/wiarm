38cfb24061fad282b4f0b18522042c9f47f0002c
Rorisang <rorisangputu@gmail.com>
2025-04-08 14:00:39 +0200

    importing involved section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import AboutSection from '../components/AboutSection'
 import Pillars from '../components/Pillars'
+import Involved from '../components/Involved'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <AboutSection/>
       <Pillars/>
+      <Involved/>
     </div>
   )
 }
