354bed31a00b8d398a6c7eec70b070cbb9e2f23b
Rorisang19 <rorisangputu@gmail.com>
2025-04-22 11:04:26 +0200

    styling logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,18 @@ import corn from '../assets/corn_img.png'
 const Header = () => {
   return (
     <div className="w-full py-2">
-      <div className="">
+      <div className="mx-auto border">
+        <div className="flex items-center gap-2">
+          <img src={corn} alt="" className="h-10 w-10" />
+          <div>
+            <h1 className="text-xl font-semibold">WIARM</h1>
+            <p className="w-28 font-light leading-3 text-[1.5vh]">Women In Agriculture Rural Movement </p>
+          </div>
+        </div>
+        {/* Mobile Menu */}
+        <div>
 
+        </div>
       </div>
     </div>
   );
