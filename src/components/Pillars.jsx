e3d1b312dc5baaf693092b270cf415fbbf9e0a45
Rorisang Putu <rorisangputu@gmail.com>
2025-04-22 20:09:41 +0200

    removing unused imports

--- src/components/Pillars.jsx
+++ src/components/Pillars.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import land from '../assets/land.webp'
-import wheat from '../assets/wheat (2).png'
-import book from '../assets/book.png'
-import earth from '../assets/earth.png'
-import sov_food from '../assets/sov_food.jpg'
-import mulch from '../assets/mulch_farming.webp'
+
 const Pillars = () => {
   return (
     <div className='w-full py-12'>
