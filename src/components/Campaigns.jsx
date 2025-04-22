28d85b4759168c641bae2eef7cd4d3d2aba1a995
Rorisang Putu <rorisangputu@gmail.com>
2025-04-22 21:26:55 +0200

    adding the first campaigns text content and image

--- src/components/Campaigns.jsx
+++ src/components/Campaigns.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import dotted from '../assets/dotted_sa_bg.png'
+import women from '../assets/two_women_farming.jpg'
 
 const Campaigns = () => {
     return (
@@ -12,7 +13,7 @@ const Campaigns = () => {
                 className="absolute inset-0 w-full h-full object-cover opacity-20 z-0"
             />
             <div className='w-[90%] mx-auto relative z-10 text-white'>
-                <div className='flex flex-col'>
+                <div className='flex flex-col gap-5'>
                     <div className="flex flex-col">
                         <div className='flex items-center gap-4 justify-center mb-2'>
                             <hr className="w-16 border-t-2 border-[#cecece]" />
@@ -35,8 +36,26 @@ const Campaigns = () => {
                             </div>
                         </div>
                     </div>
-                    <div className=''>
-
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                        <div className='py-7 flex flex-col gap-7'>
+                            <img src={women} alt="" className='w-full h-full object-cover' />
+                            <div className='flex flex-col gap-2'>
+                                <h3 className='text-3xl capitalize font-normal'>
+                                    Seeds of Resistance
+                                </h3>
+                                <ul className='list-disc list-outside flex ml-5 gap-7 font-light'>
+                                    <li>Eastern Cape, RSA</li>
+                                    <li>March 20, 2025</li>
+                                </ul>
+                                <p className='line-clamp-2'>
+                                    This campaign brings together women farmers to reclaim ancestral
+                                    land for communal farming. Through seed exchanges, soil healing
+                                    workshops, and community planting days, “Seeds of Resistance” is
+                                    reviving indigenous farming methods and strengthening local food
+                                    systems rooted in tradition and resilience.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
