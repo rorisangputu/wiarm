f5d63877ec7635cc5831e9ded8c254770cc7c437
Rorisang19 <rorisangputu@gmail.com>
2025-04-22 14:28:51 +0200

    importing mission and values component

---
+++ src/components/MissionValues.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const MissionValues = () => {
+  return (
+    <div className='w-full bg-green-900 py-10'>
+      <div className='w-[90%] mx-auto'>
+        <div>
+            <div>
+                <h1>Our Mission</h1>
+                <ul>
+                    <li>
+                        To empower Black women in rural communities to 
+                        own and work the land
+                    </li>
+                    <li>
+                        To challenge the systems that exclude us from agriculture
+                    </li>
+                    <li>
+                        To cultivate a future where our communities are 
+                        fed, sustainable, and self-sufficient
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MissionValues
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from '../components/Hero'
 import AboutSection from '../components/AboutSection'
 import Pillars from '../components/Pillars'
 import Involved from '../components/Involved'
+import MissionValues from '../components/MissionValues'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <AboutSection/>
+      <MissionValues/>
     </div>
   )
 }
