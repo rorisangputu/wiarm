811cd581057a6f504881200f74829561da3a9719
Rorisang Putu <rorisangputu@gmail.com>
2025-04-22 19:54:00 +0200

    styling text

--- src/components/Pillars.jsx
+++ src/components/Pillars.jsx
@@ -13,40 +13,40 @@ const Pillars = () => {
           <div className='flex flex-col gap-3'>
             <div className="flex items-center gap-4">
               <h1 className="uppercase font-medium tracking-[0.2em] text-green-800">
-                  Key Pillars
+                Key Pillars
               </h1>
               <hr className="w-12 border-t-2 border-gray-200" />
             </div>
-            <p>
-              At the heart of WiARM&apos;s work are the foundational pillars that 
-              guide our mission. These pillars represent the core values and strategic 
-              focus areas that help us move toward liberation through land, food, and 
+            <p className='text-slate-900'>
+              At the heart of WiARM&apos;s work are the foundational pillars that
+              guide our mission. These pillars represent the core values and strategic
+              focus areas that help us move toward liberation through land, food, and
               community.
             </p>
           </div>
-          <hr className="my-5 flex md:mx-auto w-[50%] border-t-2 border-gray-200" />
+          <hr className="my-5 flex md:mx-auto w-[30%] border-t-2 border-gray-200" />
           <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <div className='flex flex-col gap-3'>
-              <h1>Reclaiming Land</h1>
-              <p>
-                We fight for access to land and collective ownership for Black women. 
-                Land is more than property—it’s identity, livelihood, and legacy. 
+            <div className='flex flex-col gap-1 md:text-center'>
+              <h1 className='font-normal text-green-800 text-md'>Reclaiming Land</h1>
+              <p className='text-md font-light text-slate-900'>
+                We fight for access to land and collective ownership for Black women.
+                Land is more than property—it&apos;s identity, livelihood, and legacy.
                 Reclaiming land means reclaiming power
               </p>
             </div>
-            <div className='flex flex-col gap-3'>
-              <h1>Food Sovereignty</h1>
-              <p>
-                We grow, harvest, and share food within our communities. Food sovereignty 
-                gives us the right to define our food systems and ensures that nourishment 
+            <div className='flex flex-col gap-1 md:text-center'>
+              <h1 className='font-normal text-green-800 text-md'>Food Sovereignty</h1>
+              <p className='text-md font-light text-slate-900'>
+                We grow, harvest, and share food within our communities. Food sovereignty
+                gives us the right to define our food systems and ensures that nourishment
                 stays rooted in our hands.
               </p>
             </div>
-            <div className='flex flex-col gap-3'>
-              <h1>Education & Advocacy</h1>
-              <p>
-                We provide hands-on training in sustainable farming and lead policy work to break 
-                down systemic barriers. Through education and advocacy, we grow skills—and shift 
+            <div className='flex flex-col gap-1 md:text-center'>
+              <h1 className='font-normal text-green-800 text-md'>Education & Advocacy</h1>
+              <p className='text-md font-light text-slate-900'>
+                We provide hands-on training in sustainable farming and lead policy work to break
+                down systemic barriers. Through education and advocacy, we grow skills—and shift
                 systems.
               </p>
             </div>
