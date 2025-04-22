c29e4b62d441f093644fb41cc8f65f753bc9388d
Rorisang Putu <rorisangputu@gmail.com>
2025-04-22 20:25:39 +0200

    importing campaign component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Hero from '../components/Hero'
 import AboutSection from '../components/AboutSection'
 import Pillars from '../components/Pillars'
-import Involved from '../components/Involved'
 import MissionValues from '../components/MissionValues'
+import Campaigns from '../components/Campaigns'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <AboutSection/>
-      <MissionValues/>
-      <Pillars/>
+      <Hero />
+      <AboutSection />
+      <MissionValues />
+      <Pillars />
+      <Campaigns />
     </div>
   )
 }
