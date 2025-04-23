81278c0a80d382d248e22186db9fa5d29b051dec
Rorisang <rorisangputu@gmail.com>
2025-04-23 12:02:04 +0200

    adding text to about page hero

---
+++ src/components/About/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-[50vh] py-10'>
+        <div className='w-[90%] mx-auto'>
+            <div className='flex flex-col gap-2 py-16'>
+                <p className='uppercase text-xs font-medium text-green-900 tracking-[0.1em]'>
+                 Rooted in Resistance, Growing in Power.
+                </p>
+                <h1 className='capitalize text-3xl font-medium'>Reclaiming Land. Restoring Dignity. Reshaping Futures.</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
