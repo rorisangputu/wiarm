7ecd1ef6afd955a84f8852f5adad9fac674ed8f0
Rorisang <rorisangputu@gmail.com>
2025-04-23 15:40:16 +0200

    finishing touches

--- src/components/About/Hero.jsx
+++ src/components/About/Hero.jsx
@@ -3,15 +3,20 @@ import african_woman from '../../assets/african-women.jpg'
 
 const Hero = () => {
   return (
-    <div className='w-full h-[50vh] py-10 relative flex bg-gray-900 text-white'>
+    <div className='w-full h-[55vh] py-10 relative flex bg-gray-900 text-white'>
         <img src={african_woman} alt="" className="absolute inset-0 w-full h-full object-cover opacity-50"/>
         
         <div className='w-[90%] mx-auto relative z-10'>
-            <div className='flex flex-col gap-2 py-16'>
-                <p className='uppercase text-xs font-medium text-green-400 tracking-[0.1em]'>
+            <div className='flex flex-col gap-2 pt-24'>
+                <p className='uppercase text-xs font-medium text-green-500 tracking-[0.1em]'>
                  Rooted in Resistance, Growing in Power.
                 </p>
                 <h1 className='capitalize text-3xl font-medium'>Reclaiming Land. Restoring Dignity. Reshaping Futures.</h1>
+                <div className='flex justify-start mt-4'>
+                    <button className='capitalize text-[#dddddd] py-3 px-5 rounded-lg bg-[#232d25]'>
+                        Learn more
+                    </button>
+                </div>
             </div>
         </div>
     </div>
