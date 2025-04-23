17cfc47aaedf56784a1bc7346535c70b380ebb46
Rorisang <rorisangputu@gmail.com>
2025-04-23 15:53:29 +0200

    importing about component  to about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/About/Hero'
+import About from '../components/About/About'
 
 const AboutUs = () => {
   return (
     <div>
       <Hero/>
+      <About/>
     </div>
   )
 }
