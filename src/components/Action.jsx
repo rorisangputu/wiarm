109e5bda1798b7ce41a2b392d1d6924b23522d99
Rorisang <rorisangputu@gmail.com>
2025-04-23 09:30:30 +0200

    creating new action component and adding image bg

---
+++ src/components/Action.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import action_bg from '../assets/GetInvolved.png'
+
+const Action = () => {
+  return (
+    <div className='relative w-full flex items-center justify-center'>
+        <img 
+            src={action_bg} 
+            alt="" 
+            className='absolute inset-0 w-full h-full object-cover' 
+        />
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Action
