40a7976c7b85b1a02eb4f5460df29daa2bfd288a
Rorisang <rorisangputu@gmail.com>
2025-04-23 15:53:43 +0200

    scaffolding

---
+++ src/components/About/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full py-10'>
+        <div className='w-[90%] mx-auto'>
+            <div>
+                {/* About */}
+                <div>
+
+                </div>
+                {/* Mission and Values */}
+                <div>
+
+                </div>
+                {/* Team */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
