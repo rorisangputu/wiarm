1d357e47e20b66304955da63a5ef79a3b880e0b7
Rorisang19 <rorisangputu@gmail.com>
2025-04-25 15:12:07 +0200

    importing team component

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/About/Hero'
 import About from '../components/About/About'
+import MissionValues from '../components/Home/MissionValues'
+import Team from '../components/About/Team'
 
 const AboutUs = () => {
   return (
     <div>
       <Hero/>
       <About/>
+      <MissionValues/>
+      <Team/>
     </div>
   )
 }
