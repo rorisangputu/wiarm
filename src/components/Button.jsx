2ed44c2ba3d824945878995d41512c117dd62aa7
Rorisang19 <rorisangputu@gmail.com>
2025-04-29 09:10:37 +0200

    editing button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, styles}) => {
   return (
-    <button className={`flex items-center gap-2 bg-green-800 rounded-xl py-3 px-4 text-white ${styles}`}>
+    <button className={`flex items-center shadow gap-2 bg-green-800 rounded-xl py-3 px-4 text-white ${styles}`}>
       {text}
     </button>
   )
