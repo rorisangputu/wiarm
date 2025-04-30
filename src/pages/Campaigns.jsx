6f84174b9d510315c68e4a01a5f0c0626a6d0051
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 13:33:07 +0200

    importing get involved component

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Campaigns/Hero'
 import CampaignsList from '../components/Campaigns/CampaignsList'
+import Involved from '../components/Campaigns/Involved'
 const Campaigns = () => {
   return (
     <div>
         <Hero/>
         <CampaignsList/>
+        <Involved/>
     </div>
   )
 }
