ac2f6e552d641e9a0b763c4320a7c44d0cd83cc0
Rorisang Putu <rorisangputu@gmail.com>
2025-04-30 18:05:41 +0200

    changing font and adding medium screen sizes

--- src/components/Campaigns/CampaignsList.jsx
+++ src/components/Campaigns/CampaignsList.jsx
@@ -6,7 +6,7 @@ import { campaigns } from '../../data/campaignData'
 
 const CampaignsList = () => {
     return (
-        <div className='w-full bg-green-900 py-10 relative overflow-hidden'>
+        <div className='w-full bg-green-900 py-10 relative overflow-hidden font-sans'>
 
             {/* Background Image Positioned Behind Content */}
             <img
@@ -16,11 +16,11 @@ const CampaignsList = () => {
             />
             <div className='w-[90%] mx-auto relative z-10 text-white'>
                 <div className='flex flex-col gap-10'>
-                    <div className=' flex flex-col items-center gap-5'>
-                        <p className='text-center capitalize font-extralight text-lg'>
-                            Explore our most recent campaigns
+                    <div className=' flex flex-col items-center md:items-start gap-5'>
+                        <p className='font-sans text-center md:text-start font-extralight text-lg'>
+                            Explore our recent campaigns
                         </p>
-                        <hr className='w-30'/>
+                        <hr className='w-20' />
                     </div>
                     <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                         {
@@ -47,4 +47,3 @@ const CampaignsList = () => {
 export default CampaignsList
 
 
-  
