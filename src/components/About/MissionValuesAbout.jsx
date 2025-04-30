f65119fd24084055ef301b1c42d0e5e9721f0cee
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 09:17:58 +0200

    adding content

---
+++ src/components/About/MissionValuesAbout.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const MissionValuesAbout = () => {
+  return (
+    <div className='w-full bg-[#dad7cd]'>
+        <div className='py-10'>
+            <div className='w-[90%] mx-auto'>
+                <div>
+                    <div>
+                        <h1>Mission</h1>
+                        <p>
+                            To restore land access, achieve 
+                            food sovereignty, and cultivate 
+                            leadership among Black rural women 
+                            through farming, education, and advocacy.
+                        </p>
+                    </div>
+                    <div>
+                        <h1>Our Core Values</h1>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MissionValuesAbout
