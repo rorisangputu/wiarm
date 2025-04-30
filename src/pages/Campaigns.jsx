95cd4161e0058b803dfa852d32d1b794af501c48
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 11:39:00 +0200

    importing new campaign list component

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/Campaigns/Hero'
-
+import CampaignsList from '../components/Campaigns/CampaignsList'
 const Campaigns = () => {
   return (
     <div>
         <Hero/>
+        <CampaignsList/>
     </div>
   )
 }
