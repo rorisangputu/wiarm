b9c90937ed495787bc01d1fa7068dd4c77b5253b
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 09:44:21 +0200

    commit

--- src/components/About/MissionValuesAbout.jsx
+++ src/components/About/MissionValuesAbout.jsx
@@ -34,7 +34,6 @@ const MissionValuesAbout = () => {
                             </div>
                             ))}
                         </div>
-
                     </div>
                 </div>
             </div>
