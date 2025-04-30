da3ca4e578925b2d2cab33cf248fe6290c8a2823
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 14:16:05 +0200

    adding styling for medium screens

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -15,13 +15,13 @@ const Hero = () => {
 
       {/* Content */}
       <div className="top-4 relative z-10 text-center px-6">
-        <h1 className="text-4xl font-semibold md:text-5xl">Agriculture for Liberation</h1>
-        <p className="mt-3 font-sans font-normal text-md md:text-xl max-w-2xl mx-auto">
+        <h1 className="text-4xl font-semibold md:text-6xl">Agriculture for Liberation</h1>
+        <p className="mt-3 font-sans font-normal text-md md:text-2xl max-w-2xl mx-auto">
           Reclaiming land, food sovereignty, and our future.
           Join us in building a just and sustainable agricultural movement.
         </p>
 
-        <HeroButton styles={"font-sans mt-6 px-12 py-3 bg-green-800 hover:bg-green-900 text-white font-medium rounded-xl transition duration-300"}/>
+        <HeroButton styles={"font-sans mt-6 px-12 md:px-16 py-3 md:py-5 md:text-xl bg-green-800 hover:bg-green-900 text-white font-medium rounded-xl transition duration-300"}/>
       </div>
     </div>
 
