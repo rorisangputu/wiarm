735e64f5f2b06f72a25137c4725927d4fe576643
Rorisang19 <rorisangputu@gmail.com>
2025-04-30 14:55:18 +0200

    styles for medium screens

--- src/components/BlackButton.jsx
+++ src/components/BlackButton.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const BlackButton = ({text}) => {
   return (
-    <button className="flex capitalize font-light text-md items-center gap-2 bg-[#232d25] rounded-lg py-3 px-4 text-white">
+    <button className="flex capitalize font-light text-md md:text-lg items-center gap-2 
+      bg-[#232d25] rounded-lg py-3 px-4 text-white md:py-4 md:px-6">
         {text}
     </button>
   )
